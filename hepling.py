5aa8fbdaf42c6cd554e06834647aad636b2e6840
Oleksandra Panova <qqqwm@yandex.ua>
2017-06-17 21:30:30 +0300

    -
    _

--- hepling.py
+++
@@ -1,7 +0,0 @@
-def is_word_guessed(secret_word, letters_guessed):
-    for i in range (0, len(secret_word)):
-        if secret_word[i] != letters_guessed[i]:
-            return False
-    return True
-        
-print(is_word_guessed('app', ['y', 'p', 'p']))
